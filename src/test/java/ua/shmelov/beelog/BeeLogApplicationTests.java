81480a4ba92ef8d45a73ca04d8a5ec96e17a21cb
Oleksandr Shmelov <shmelov.aleksandr@gmail.com>
2025-07-18 17:40:08 +0200

    Initial commit

---
+++ src/test/java/ua/shmelov/beelog/BeeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.shmelov.beelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
